162ddce1f4318628fa8e4106aaaea0c0ad738ff6
hhh <hanzalakhokhar@gmail.com>
2023-01-31 14:25:59 +0500

    31-01-2023

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { TextInput } from "react-native-paper";
 import { colors } from "../utils/colors";
+import { RoundedButton } from "../components/RoundedButton";
 
-export const Focus = () => (
-  <View style={styles.container}>
-    <View style={styles.inputContainer}>
-      <TextInput label="What would like to focus on?" />
+export const Focus = () => {
+  const [subject, setSubject] = useState(null);
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setSubject}
+          label="What would like to focus on?"
+        />
+        <View style={styles.button}>
+          <RoundedButton title="+" size={50} />
+        </View>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   inputContainer: {
-    flex: 0.5,
-    padding: 25,
+    padding: 25, 
     justifyContent: "top",
+    flexDirection: "row",
   },
   text: {
     color: colors.white,
   },
+  textInput: {
+    flex: 1,
+    marginRight: 10,
+  },
+  button: {
+    justifyContent: "center",
+  },
 });
-
 
 // ==== Folder : 4 || Video : 17 =================================
